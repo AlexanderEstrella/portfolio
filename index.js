4a5e4f554f65abe65d05cd45c4aaa48d06e499de
AlexanderEstrella <alexanderestrella23@gmail.com>
2023-06-18 00:12:44 -0400

    added new function to home page rendering name

--- index.js
+++ index.js
@@ -9,3 +9,26 @@ app = () => {
     );
   });
 };
+// windows load calls function shiftname
+window.onload = function () {
+  shiftName();
+};
+// push  name in inner html using loop and DOM.
+function shiftName() {
+  const name = document.getElementsByClassName("subheading")[0];
+  let emptyName = "";
+  const string = "I'm Alexander";
+
+  for (let i = 0; i < string.length; i++) {
+    time(i);
+  }
+
+  function time(i) {
+    setTimeout(() => {
+      emptyName += string[i];
+      name.innerHTML = emptyName;
+    }, 1000 * i); // Multiply the delay by 'i' to increment the delay for each iteration
+  }
+}
+
+$(window).on("scroll", function () {});
