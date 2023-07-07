b549feeb0e3c89570cf53e83989f8cd7f22f9770
AlexanderME <97694971+AlexanderEstrella@users.noreply.github.com>
2023-07-07 16:28:42 -0400

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -2,4 +2,17 @@ export const Data = () => ({
   id: 1,
   description: "short desk",
   img: "/images/quotegen",
-});
+},
+      {
+      id: 1,
+      title: "Project 1",
+      description: "https://lockitdown-8c2b76d46fff.herokuapp.com/",
+      image: "project1.jpg",
+    },
+    {
+      id: 2,
+      title: "Project 2",
+      description: "https://gmnailsorlando.netlify.app/",
+      image: "project2.jpg",
+    },                    
+                          );
